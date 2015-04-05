5ec6a0e503ab5b5540b070389e79feae7666325f
Ajay Karpur <ajay@ebookglue.com>
2015-04-05 20:32:07 +0800

    moved everything into main directory (except lib)
    because fuck being organized

---
+++ test.bat
@@ -0,0 +1,2 @@
+python index.py -i ..\patsnap-corpus -d dictionary.txt -p postings.txt 1> index.console.txt 2> index.log.txt
+REM python search.py -d dictionary.txt -p postings.txt -q q1.xml -o output.txt 1> search.console.txt 2> search.log.txt
